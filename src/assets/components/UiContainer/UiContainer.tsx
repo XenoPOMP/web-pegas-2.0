76a3467a2ea0dc28952869e3b06648807c403adb
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-24 00:39:30 +0300

    update UiContainer component

--- src/assets/components/UiContainer/UiContainer.tsx
+++ src/assets/components/UiContainer/UiContainer.tsx
@@ -4,8 +4,18 @@ import { FC, PropsWithChildren } from 'react';
 import styles from './UiContainer.module.scss';
 import type { UiContainerProps } from './UiContainer.props';
 
-const UiContainer: FC<PropsWithChildren<UiContainerProps>> = ({ children }) => {
+const UiContainer: FC<PropsWithChildren<unknown>> & {
+	Section: FC<PropsWithChildren<unknown>>;
+	Article: FC<PropsWithChildren<unknown>>;
+} = ({ children }) => {
 	return <div className={cn(styles.container)}>{children}</div>;
 };
+
+UiContainer.Section = ({ children }) => (
+	<section className={cn(styles.container)}>{children}</section>
+);
+UiContainer.Article = ({ children }) => (
+	<article className={cn(styles.container)}>{children}</article>
+);
 
 export default UiContainer;
--- src/assets/pages/MainPage/MainPage.tsx
+++ src/assets/pages/MainPage/MainPage.tsx
@@ -1,18 +1,14 @@
 import cn from 'classnames';
 
 import Page from '@components/Page/Page';
-import UiContainer from '@components/UiContainer/UiContainer';
-import UiGrid from '@components/UiGrid/UiGrid';
 
 import styles from './MainPage.module.scss';
 
 const MainPage = () => {
 	return (
-		<Page meta={{ pageTitle: 'Main', pageDescription: '', keywords: '' }}>
-			<UiGrid.Section cols={12} rows={3}>
-				123
-			</UiGrid.Section>
-		</Page>
+		<Page
+			meta={{ pageTitle: 'Main', pageDescription: '', keywords: '' }}
+		></Page>
 	);
 };
 
