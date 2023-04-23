65dfabbc2857ab3f243f15529e83fd4dfa679565
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-24 01:55:39 +0300

    add banner to Header

--- src/assets/components/ui/Header/Header.tsx
+++ src/assets/components/ui/Header/Header.tsx
@@ -2,10 +2,12 @@ import cn from 'classnames';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
+import Portal from '@components/Portal/Portal';
 import UiContainer from '@components/UiContainer/UiContainer';
 
 import Navbar from '@ui/Navbar/Navbar';
 
+import banner from '@media/backgrounds/space.png';
 import logotype from '@media/branding/logo.png';
 
 import styles from './Header.module.scss';
@@ -13,25 +15,36 @@ import type { HeaderProps } from './Header.props';
 
 const Header: FC<HeaderProps> = ({}) => {
 	return (
-		<header className={cn(styles.appHeader)}>
-			<UiContainer className={cn(styles.headerContainer)}>
-				<Link to={'/'}>
-					<img className={cn(styles.logo)} src={logotype} alt={'logo'} />
-				</Link>
+		<>
+			<header className={cn(styles.appHeader)}>
+				<UiContainer className={cn(styles.headerContainer)}>
+					<Link to={'/'}>
+						<img className={cn(styles.logo)} src={logotype} alt={'logo'} />
+					</Link>
 
-				<Navbar />
+					<Navbar />
 
-				<Link
-					to={'/'}
-					style={{
-						opacity: 0,
-						pointerEvents: 'none',
-					}}
-				>
-					<img className={cn(styles.logo)} src={logotype} alt={'logo'} />
-				</Link>
-			</UiContainer>
-		</header>
+					<Link
+						to={'/'}
+						style={{
+							opacity: 0,
+							pointerEvents: 'none',
+						}}
+					>
+						<img className={cn(styles.logo)} src={logotype} alt={'logo'} />
+					</Link>
+				</UiContainer>
+			</header>
+
+			<div
+				className={cn(styles.banner)}
+				style={{
+					background: `url(${banner}) center`,
+				}}
+			>
+				<UiContainer>12</UiContainer>
+			</div>
+		</>
 	);
 };
 
