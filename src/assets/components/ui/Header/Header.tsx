9ad8c5fd9cb8ee3fd9bfb682971bdf35a83098ed
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-25 16:03:46 +0300

    add pegas to Header`s banner

--- src/assets/components/ui/Header/Header.tsx
+++ src/assets/components/ui/Header/Header.tsx
@@ -7,10 +7,12 @@ import UiGrid from '@components/UiGrid/UiGrid';
 
 import BurgerNavbar from '@ui/BurgerNavbar/BurgerNavbar';
 import Navbar from '@ui/Navbar/Navbar';
+import ProgressiveImage from '@ui/ProgressiveImage/ProgressiveImage';
 import MediaWidthView from '@ui/mediaQueries/MediaWidthView/MediaWidthView';
 
 import banner from '@media/backgrounds/space.png';
 import logotype from '@media/branding/logo.png';
+import pegas from '@media/images/unicorn.png';
 
 import styles from './Header.module.scss';
 import type { HeaderProps } from './Header.props';
@@ -53,7 +55,31 @@ const Header: FC<HeaderProps> = ({}) => {
 				}}
 			>
 				<UiGrid.Section className={cn(styles.bannerContainer)}>
-					<div className={cn(styles.textBlock)}>1</div>
+					<div className={cn(styles.textBlock)}>
+						<h2>IT-компания “Веб-Пегас” приветствует вас.</h2>
+
+						<div className={cn(styles.underHeader)}>
+							Мы небольшая команда опытных и талантливых специалистов, которая
+							ценится своей надежностью, ответственность и уникальными подходами
+							к решению ваших задач или проблем.
+						</div>
+
+						<div className={cn(styles.seeMore)}>Подробнее</div>
+					</div>
+
+					<div className={cn(styles.pegasBlock)}>
+						<div className={cn(styles.circle)}>
+							<div className={cn(styles.pegasPlaceholder)}>
+								<ProgressiveImage
+									src={pegas}
+									loaderColorScheme={{
+										backgroundColor: 'trasparent',
+										loaderColor: 'white',
+									}}
+								/>
+							</div>
+						</div>
+					</div>
 				</UiGrid.Section>
 			</div>
 		</>
