0cd503887f9f8a5376a886b7e522001b0b153ee9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-27 00:22:09 +0300

    add patch under ServiceSection

--- src/assets/components/sections/ServiceSection/ServiceSection.tsx
+++ src/assets/components/sections/ServiceSection/ServiceSection.tsx
@@ -1,8 +1,12 @@
 import cn from 'classnames';
 import { FC } from 'react';
 
+import Portal from '@components/Portal/Portal';
 import UiGrid from '@components/UiGrid/UiGrid';
 
+import ProgressiveImage from '@ui/ProgressiveImage/ProgressiveImage';
+
+import patchImg from '@media/images/underServicePatch.svg';
 import cardZero from '@media/services/service-card-0.svg';
 import cardOne from '@media/services/service-card-1.svg';
 
@@ -11,15 +15,41 @@ import type { ServiceSectionProps } from './ServiceSection.props';
 
 const ServiceSection: FC<ServiceSectionProps> = ({}) => {
 	return (
-		<UiGrid.Section className={cn(styles.service)}>
-			<h2>Услуги</h2>
+		<>
+			<UiGrid.Section className={cn(styles.service)}>
+				<h2>Услуги</h2>
 
-			<div className={cn(styles.services)}>
-				<img src={cardZero} alt={'card-zero'} />
+				<div className={cn(styles.services)}>
+					<ProgressiveImage
+						loaderColorScheme={{
+							backgroundColor: 'transparent',
+							loaderColor: 'black',
+						}}
+						src={cardZero}
+						alt={'card-zero'}
+					/>
 
-				<img src={cardOne} alt={'card-one'} />
+					<ProgressiveImage
+						loaderColorScheme={{
+							backgroundColor: 'transparent',
+							loaderColor: 'black',
+						}}
+						src={cardOne}
+						alt={'card-one'}
+					/>
+				</div>
+			</UiGrid.Section>
+
+			<div className={cn(styles.underServicePatch)}>
+				<img src={patchImg} alt={'patch'} />
+
+				<Portal enabled={false}>
+					<div className={cn(styles.wordGroup)}>
+						<div className={cn(styles.word)}>{`</Web Pegas>`}</div>
+					</div>
+				</Portal>
 			</div>
-		</UiGrid.Section>
+		</>
 	);
 };
 
