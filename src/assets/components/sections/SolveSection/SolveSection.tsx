acd50a0d473f8071837ffc1f950a5b372bf7696b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-28 13:42:53 +0300

    update SolveSection

--- src/assets/components/sections/SolveSection/SolveSection.tsx
+++ src/assets/components/sections/SolveSection/SolveSection.tsx
@@ -13,11 +13,43 @@ const SolveSection: FC<SolveSectionProps> = ({}) => {
 			<h2>Решение задач</h2>
 
 			<UiGrid className={cn(styles.cardGroup)} maxWidth={'unset'}>
-				<UiGrid className={cn(styles.content)} maxWidth={'unset'}>
-					<div className={cn(styles.card)}></div>
-					<div className={cn(styles.card)}></div>
-					<div className={cn(styles.card)}></div>
-				</UiGrid>
+				<div className={cn(styles.content)}>
+					<div className={cn(styles.card)}>
+						<div className={cn(styles.container)}>
+							<h3>Бриф</h3>
+
+							<div className={cn(styles.text)}>
+								Стоит отметить, если же вы не нашли подходящую вам услугу или не
+								можете закончить формулирование вашей идеи, то мы окажем вам
+								наиболее эффективное сотрудничество.
+							</div>
+						</div>
+					</div>
+					<div className={cn(styles.card, styles.reversed)}>
+						<div className={cn(styles.container)}>
+							<h3>Решение</h3>
+
+							<div className={cn(styles.text)}>
+								Далее для решения мы озадачим наших лучших специалистов, как и
+								на территории Российской Федерации, так и других страны, которые
+								имеют огромный опыт для составления более результативного
+								решения.
+							</div>
+						</div>
+					</div>
+					<div className={cn(styles.card)}>
+						<div className={cn(styles.container)}>
+							<h3>Результат</h3>
+
+							<div className={cn(styles.text)}>
+								Мы рассчитываем на доверительное и долгосрочное сотрудничество
+								без привлечения третьих лиц в наши с вами взаимоотношения, в
+								основе которого будем сохранять политику конфиденциальности
+								обеих сторон.
+							</div>
+						</div>
+					</div>
+				</div>
 			</UiGrid>
 		</UiContainer.Section>
 	);
